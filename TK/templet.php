d89859d9450ec69c509996823dc6be7b72c31a7a
saras117 <sarasbidot@gmail.com>
2016-11-25 12:21:56 +0700

    Add files via upload
    templet doang

---
+++ TK/templet.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="TK Basdat">
+		<meta name="author" content="B04">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" type= "text/css" href="css/main.css">
+		<script src="script/bootstrap.min.js"></script>
+		<title>SI Asisten</title>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>                        
+				</button>
+				<a class="navbar-brand" href="#">SI Asisten</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Menu 1</a></li>
+				<li><a href="#">Menu 2</a></li>
+				<li><a href="#">Menu 3</a></li>
+			  </ul>
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a href="#"> Login</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+  
+		<div class="container-fluid text-center">    
+			<div class="row content">
+				<div class="col-sm-2 sidenav">
+				</div>
+				
+				
+				<div class="col-sm-8 text-left midit"> 
+					<h1>Tabel</h1>
+					<div class="midit">
+						<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>A</th>
+									<th>B</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>A</td>
+									<td>B</td>
+								</tr>
+								<tr>
+									<td>A</td>
+									<td>B</td>
+								</tr>
+								<tr>
+									<td>A</td>
+									<td>B</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					
+				</div>
+				
+				
+				<div class="col-sm-2 sidenav">
+				</div>
+			</div>
+		</div>
+
+		<footer class="container-fluid text-center">
+			<p>Kelompok B-04</p>
+		</footer>		
+	</body>
+</html>
